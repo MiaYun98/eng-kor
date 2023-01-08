ca24a1514b3f2269118d77ce514c2f4b4a2ba8cd
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-01-07 20:50:09 -0800

    big

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// page and component import 
+import Home from "./pages/Home"
+import Navbar from "./components/navbar";
+import "./style.css"
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+// style
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle"
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router basename="/">
+        {/* NavBar will go over here */}
+        <Navbar/>
+        <Routes>
+          {/* Home route */}
+          <Route path="/" element={<Home />}></Route>
+          {/* About route */}
+          <Route path="about" element={<About/>}></Route>
+          {/* Contact route? */}
+          <Route path="contact" element={<Contact/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
