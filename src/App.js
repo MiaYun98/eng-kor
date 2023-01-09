1f303c724f2897a2e9ce965b28030504650c01a6
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-01-09 15:41:04 -0800

    home setting done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import { React } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Home from './components/Home'
 
-// page and component import 
-import Home from "./pages/Home"
-import Navbar from "./components/navbar";
-import "./style.css"
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-
-// style
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle"
-
-function App() {
-
-  return (
-    <div className="App">
-      <Router basename="/">
-        {/* NavBar will go over here */}
-        <Navbar/>
-        <Routes>
-          {/* Home route */}
-          <Route path="/" element={<Home />}></Route>
-          {/* About route */}
-          <Route path="about" element={<About/>}></Route>
-          {/* Contact route? */}
-          <Route path="contact" element={<Contact/>}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
-}
+const App = () => <Home />;
 
 export default App;
+
