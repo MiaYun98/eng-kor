0ce7a7f224aede036fc4034fc02b35fbe1fe3048
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-01-11 21:26:35 -0800

    working

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,13 +3,14 @@ import { React, useState } from 'react'
 export default function Home() {
     const [textInput, setTextInput] = useState("");
 
-
     return (
         <div>
-            <p>home</p>
-            <input type="text" name="sentence" placeholder="Sentence you want to translate into Korean" onChange={e => setTextInput(e.target.value.trim())}></input>
-
+            <form onSubmit={getSentence}>
+                <input type="text" name="sentence" placeholder="Sentence you want to translate into Korean" onChange={e => setTextInput(e.target.value.trim())}></input>
+            </form>
+            
             <input type="text" name="sentence" placeholder="Translated Korean will be shown here" onChange={e => setTextInput(e.target.value.trim())}></input>
+
         </div>
     )
 }
