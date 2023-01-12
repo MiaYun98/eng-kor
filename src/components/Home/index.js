f91f2510d85fa870a45f2ddb1a914388698548d5
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-01-12 11:39:37 -0800

    font and background + text input working as I intended

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,21 @@
 import { React, useState } from 'react'
+import "./style.css"
 
 export default function Home() {
     const [textInput, setTextInput] = useState("");
+    const [translate, setTranslate] = useState("");
 
+    const getSentence = () => {
+        console.log(textInput)
+        setTranslate("안녕")
+    }
+    
     return (
         <div>
-            <form onSubmit={getSentence}>
-                <input type="text" name="sentence" placeholder="Sentence you want to translate into Korean" onChange={e => setTextInput(e.target.value.trim())}></input>
-            </form>
-            
-            <input type="text" name="sentence" placeholder="Translated Korean will be shown here" onChange={e => setTextInput(e.target.value.trim())}></input>
+            <textarea type="text" name="sentence" placeholder="Sentence you want to translate into Korean" onChange={e => setTextInput(e.target.value.trim())}></textarea>
+            <button onClick={getSentence}></button>
 
+            <p className='translate'>{translate}</p>
         </div>
     )
 }
