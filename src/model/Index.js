1b8d049fc17dbdb9a8179785429573ee197d1e24
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-01-23 21:31:39 -0800

    comment

--- src/model/Index.js
+++ src/model/Index.js
@@ -2,6 +2,7 @@ const Noun = require("./Noun")
 const Verb = require("./Verb")
 const Adverb = require("./Adverb")
 
+// this is all the meterials (words type of the database)
 module.exports = {
     Noun,
     Verb,
