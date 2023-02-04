dd894c79d7e76873b5db6fa90894ec88ea63b239
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-02-03 19:49:46 -0800

    typo)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home'
 function App() {
     return (
         <div className="App">
-            <header>Non-Formal English to Korean translater</header>
+            <header>Non-Formal English to Korean translator</header>
             <Home/>
             <footer>Created by Sangmi Contact: sangmiyun0529@gmail.com</footer>
         </div>
