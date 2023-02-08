96a38113d1b32c707b4312490ba0684a815f9eb6
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-02-08 11:20:48 -0800

    devve

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     return (
         <div className="App">
             <header>Non-Formal English to Korean translator</header>
+            {/* developing */}
             <p className="continue">Still developing</p>
             <Home/>
             <footer>Created by Sangmi Contact: sangmiyun0529@gmail.com</footer>
