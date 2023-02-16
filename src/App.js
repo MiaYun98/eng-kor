8d8239de6761d3ebe48a6467ab0e35ee88819b99
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-02-15 19:22:48 -0800

    des

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
             <header>Non-Formal English to Korean translator</header>
-            {/* developing should work on the API or find API for the definition */}
+            {/* developing should work on the API or find API for the definition it is going to be more realted to the sytax for the word structure but the other word is diff*/}
             <p className="continue">Still developing</p>
             <Home/>
             <footer>Created by Sangmi Contact: sangmiyun0529@gmail.com</footer>
